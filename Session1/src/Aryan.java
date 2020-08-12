93ef1c2c1baa8c6c37fd6c317e6a6569932a0fd7
Aryan <Aryan@DESKTOP-KDVGJKJ>
2020-08-12 12:59:19 +0530

    Aryan Shah 11704217 Assignment1

---
+++ Session1/src/Aryan.java
@@ -0,0 +1,9 @@
+
+public class Aryan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\u0906"+"\u092F"+"\u0945"+"\u0928"+" "+"\u0936"+"\u093E"+"\u0939");
+	}
+
+}
